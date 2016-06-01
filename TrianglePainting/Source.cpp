d2c35e0b8af3ec7fd61ba163a93d5d485e53b3ed
Radi <radostina.dimovaa@gmail.com>
2016-06-01 18:11:00 +0300

    add test scene

--- TrianglePainting/Source.cpp
+++ TrianglePainting/Source.cpp
@@ -1,9 +1,13 @@
 #include "SDLWrapper.h"
 #include <stdio.h>
+#include<vector>
+#include "Random.h"
+
+int MaxTriangles = 50;
 
 int main(int argc, char *argv[])
 {
-	SDLWrapper sdl(700, 700);
+	SDLWrapper sdl(900, 500);
 	if (!sdl.initSDL())
 	{
 		return -1;
@@ -12,9 +16,29 @@ int main(int argc, char *argv[])
 	sdl.drawImage("../Images/cookie_monster.png");
 	sdl.update();
 
-	sdl.drawTirangle({ { 10,10 },{ 20, 20 },{ 100,20 } }, { 255, 0, 0 });
+	//sdl.drawTirangle({ { 500,10 },{ 200, 200 },{ 100,200 } }, { 255, 0, 0 , 10});
+
+	std::vector<Triangle> triangles;
+	Random rnd;
+	for (int i = 0; i < MaxTriangles; i++)
+	{
+		Triangle tr = rnd.getRandomTriangle(500, 500);
+		triangles.push_back(tr);
+	}
+
+	for (auto tr : triangles)
+	{
+		sdl.drawTirangle(tr);
+	}
 
 	sdl.update();
+
+
 	while (1) { SDL_Delay(1); }
 	return 0;
+}
+
+void GenerateFirstGeneration()
+{
+
 }
