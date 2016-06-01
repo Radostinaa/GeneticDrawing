bffcd17cce41a2225c6ada8bd1421a3b11b0c56e
Radi <radostina.dimovaa@gmail.com>
2016-06-01 16:50:46 +0300

    add Triangle structure

--- TrianglePainting/Vector.h
+++ TrianglePainting/Vector.h
@@ -53,18 +53,25 @@ inline Vector2 operator ^ (const Vector2& a, const Vector2& b)
 class Color
 {
 public:
-	Uint8 r, g, b;
+	Uint8 r, g, b, a;
 
-	Color(const Uint8 _r, const Uint8 _g, const Uint8 _b) : r(_r), g(_g), b(_b) {}
+	Color(const Uint8 _r, const Uint8 _g, const Uint8 _b, const Uint8 _a) : r(_r), g(_g), b(_b), a(_a) {}
 
 };
 
 inline Color operator + (const Color& a, const Color& b)
 {
-	return Color(a.r + b.r, a.g + b.g, a.b + b.b);
+	return Color(a.r + b.r, a.g + b.g, a.b + b.b, a.a + b.a);
 }
 
 inline Color operator - (const Color& a, const Color& b)
 {
-	return Color(a.r - b.r, a.g - b.g, a.b - b.b);
+	return Color(a.r - b.r, a.g - b.g, a.b - b.b, a.a - b.a);
 }
+
+
+struct Triangle
+{
+	Vector2 v1, v2, v3;
+	Triangle(Vector2 _v1, Vector2 _v2, Vector2 _v3) : v1(_v1), v2(_v2), v3(_v3) {}
+};
